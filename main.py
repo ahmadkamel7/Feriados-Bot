08e32366ecf853cc5b88b4238ba7435d005ea091
Ahmad <85586031+ahmadkamel7@users.noreply.github.com>
2023-11-16 10:48:03 -0300

    atualização tweepy
    Adaptação às novas atualizações presentes no tweepy

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import tweepy
 import keys
 import mensagem
 
-
+#MÉTODO ANTIGO
+"""
 def api(): #comandos necessários para se conectar ao servidor do twitter
     auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret)
     auth.set_access_token(keys.access_token, keys.access_token_secret)
@@ -15,3 +16,11 @@ def tweet(api: tweepy.API, message: str): # função que faz o tweet
 if __name__ == '__main__':
     api = api()
     tweet(api, mensagem.bot_message()) #chama a função que faz tweet no main!
+"""
+
+#MÉTODO NOVO (16/11/2023)
+if __name__ == '__main__':
+    client = tweepy.Client(keys.bearer_token, keys.api_key, keys.api_secret, keys.access_token, keys.access_token_secret)
+    auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret, keys.access_token, keys.access_token_secret)
+    api = tweepy.API(auth)
+    client.create_tweet(text = mensagem.bot_message())
